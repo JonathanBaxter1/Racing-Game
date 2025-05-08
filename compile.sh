61e487ca39c0b455584d8873b9f1a32fe8312e10
Jonathan Baxter <jonathanbaxter8888@gmail.com>
2025-05-08 09:42:16 -0400

    edited compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ ./src/*.cpp -o ./bin/race -march=native -Wall -lGL -lGLU -lglut -lGLEW -lglfw -lXxf86vm -lXrandr -lXi -ldl -lXinerama -lXcursor -lassimp
+g++ ./src/*.cpp -o ./bin/race -Wall -lGL -lGLU -lglut -lGLEW -lglfw -lXxf86vm -lXrandr -lXi -ldl -lXinerama -lXcursor -lassimp
