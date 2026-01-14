d2f55f6cc5a20cbb171ebb13ca0feae25095d4a3
Jonathan Baxter <jonathanbaxter8888@gmail.com>
2026-01-14 16:30:03 -0500

    optimized terrain normal variance calcs, from 50ms -> 12ms

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -15,8 +15,6 @@ Terrain::Terrain(Shader* shader, Shader* occluderShader, TextureArray* mapArray,
 	int mapWidth = (float)mapSize;
 	int mapHeight = (float)mapSize;
 
-	float waterCullTime = glfwGetTime();
-
 	unsigned int numTestPoints = (patchSize*patchSize)>>4;
 	unsigned short waterHeightShort = WATER_HEIGHT*65536.0/274.0;
 	unsigned char log2MapSize = (unsigned char)(std::log2(mapSize));
@@ -85,8 +83,6 @@ Terrain::Terrain(Shader* shader, Shader* occluderShader, TextureArray* mapArray,
 		numPatches += (unsigned int)patchAboveWater;
 	}
 
-	std::cout<<"Water cull: "<<1000*(glfwGetTime()-waterCullTime)<<"ms"<<std::endl;
-
 	float patchMinX[patchRes*patchRes] = {0.0};
 	float patchMinY[patchRes*patchRes] = {0.0};
 	// Create occluders
@@ -128,33 +124,34 @@ Terrain::Terrain(Shader* shader, Shader* occluderShader, TextureArray* mapArray,
 		patchMinY[curPatch] = patchMinY[curPatch] > WATER_HEIGHT ? patchMinY[curPatch] : WATER_HEIGHT;
 	}
 
-	std::cout<<glfwGetTime()<<" terrain normal variance:"<<std::endl;
+	float normalTime = glfwGetTime();
+
 	// Normal Variance Calcs (for terrain LOD)
 	for (unsigned int curPatch = 0; curPatch < patchRes*patchRes; curPatch++) {
 		if (!patchResolutions[curPatch]) continue;
-		unsigned int xStart = curPatch%patchRes;
-		unsigned int yStart = curPatch/patchRes;
-		unsigned int numPoints = (patchSize + 1)*(patchSize + 1);
+		unsigned int xStart = (curPatch%patchRes)*patchSize;
+		unsigned int yStart = (curPatch/patchRes)*patchSize;
 
 		// Calculate average normal value
 		unsigned int redSum = 0;
 		unsigned int greenSum = 0;
 		unsigned int blueSum = 0;
+		float redAverage, greenAverage, blueAverage;
 		float maxNormalVariance = 0.0;
-		for (unsigned int i = 0; i < numPoints; i++) {
-			unsigned int dx = i%(patchSize + 1);
-			unsigned int dy = i/(patchSize + 1);
-			unsigned int x = xStart*patchSize + dx;
-			unsigned int y = yStart*patchSize + dy;
-			x = x < (unsigned int)mapWidth - 1 ? x : (unsigned int)mapWidth - 1;
-			y = y < (unsigned int)mapHeight - 1 ? y : (unsigned int)mapHeight - 1;
+
+		// Pass 1
+		for (unsigned int i = 0; i < numTestPoints; i++) {
+			unsigned int dx = (i%(patchSize>>2))<<2;
+			unsigned int dy = (i/(patchSize>>2))<<2;
+			unsigned int x = xStart + dx;
+			unsigned int y = yStart + dy;
 			unsigned int offset = 3*(y*mapWidth + x);
 			redSum += (unsigned int)normalMap[offset + 0];
 			greenSum += (unsigned int)normalMap[offset + 1];
 			blueSum += (unsigned int)normalMap[offset + 2];
-			float redAverage = (float)redSum/(float)(i + 1);
-			float greenAverage = (float)greenSum/(float)(i + 1);
-			float blueAverage = (float)blueSum/(float)(i + 1);
+			redAverage = (float)redSum/(float)(i + 1);
+			greenAverage = (float)greenSum/(float)(i + 1);
+			blueAverage = (float)blueSum/(float)(i + 1);
 			float dRed = redAverage - (float)normalMap[offset + 0];
 			float dGreen = greenAverage - (float)normalMap[offset + 1];
 			float dBlue = blueAverage - (float)normalMap[offset + 2];
@@ -162,6 +159,55 @@ Terrain::Terrain(Shader* shader, Shader* occluderShader, TextureArray* mapArray,
 			maxNormalVariance = normalVariance > maxNormalVariance ? normalVariance : maxNormalVariance;
 			if (maxNormalVariance >= 6400.0) break;
 		}
+
+		// Pass 2
+		if (maxNormalVariance < 6400) {
+		for (unsigned int i = 0; i < patchSize + 1; i++) {
+			unsigned int x = xStart + i;
+			unsigned int y = yStart + patchSize;
+			x = x < (unsigned int)mapWidth - 1 ? x : (unsigned int)mapWidth - 1;
+			y = y < (unsigned int)mapHeight - 1 ? y : (unsigned int)mapHeight - 1;
+			unsigned int offset = 3*(y*mapWidth + x);
+			float dRed = redAverage - (float)normalMap[offset + 0];
+			float dGreen = greenAverage - (float)normalMap[offset + 1];
+			float dBlue = blueAverage - (float)normalMap[offset + 2];
+			float normalVariance = dRed*dRed + dGreen*dGreen + dBlue*dBlue;
+			maxNormalVariance = normalVariance > maxNormalVariance ? normalVariance : maxNormalVariance;
+			if (maxNormalVariance >= 6400.0) break;
+		} }
+
+		// Pass 3
+		if (maxNormalVariance < 6400) {
+		for (unsigned int i = 0; i < patchSize + 1; i++) {
+			unsigned int x = xStart + patchSize;
+			unsigned int y = yStart + 1;
+			x = x < (unsigned int)mapWidth - 1 ? x : (unsigned int)mapWidth - 1;
+			y = y < (unsigned int)mapHeight - 1 ? y : (unsigned int)mapHeight - 1;
+			unsigned int offset = 3*(y*mapWidth + x);
+			float dRed = redAverage - (float)normalMap[offset + 0];
+			float dGreen = greenAverage - (float)normalMap[offset + 1];
+			float dBlue = blueAverage - (float)normalMap[offset + 2];
+			float normalVariance = dRed*dRed + dGreen*dGreen + dBlue*dBlue;
+			maxNormalVariance = normalVariance > maxNormalVariance ? normalVariance : maxNormalVariance;
+			if (maxNormalVariance >= 6400.0) break;
+		} }
+
+		// Pass 4 (Main pass)
+		if (maxNormalVariance < 6400) {
+		for (unsigned int y = 0; y < patchSize; y++) {
+			unsigned int yOffset = ((yStart + y) << log2MapSize) + xStart;
+			for (unsigned int x = 0; x < patchSize; x++) {
+				unsigned int offset = 3*(yOffset + x);
+				float dRed = redAverage - (float)normalMap[offset + 0];
+				float dGreen = greenAverage - (float)normalMap[offset + 1];
+				float dBlue = blueAverage - (float)normalMap[offset + 2];
+				float normalVariance = dRed*dRed + dGreen*dGreen + dBlue*dBlue;
+				maxNormalVariance = normalVariance > maxNormalVariance ? normalVariance : maxNormalVariance;
+				if (maxNormalVariance >= 6400.0) break;
+			}
+		} }
+
+
 		if (maxNormalVariance < 400.0) {
 			patchResolutions[curPatch] = 8.0;
 		} else if (maxNormalVariance < 1600.0) {
@@ -171,6 +217,7 @@ Terrain::Terrain(Shader* shader, Shader* occluderShader, TextureArray* mapArray,
 		}
 
 	}
+	std::cout<<"Normal calcs: "<<1000*(glfwGetTime()-normalTime)<<"ms"<<std::endl;
 
 	unsigned int surfaceVerticesSize = 12*numPatches*sizeof(float);
 	float* surfaceVertices = (float*)malloc(surfaceVerticesSize);
