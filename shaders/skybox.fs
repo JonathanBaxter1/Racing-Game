46a00d3fca94c08d1e450b9a6925b9ee1b131030
Jonathan Baxter <jonathanbaxter8888@gmail.com>
2026-01-19 00:32:12 -0500

    fixed skybox glitch issues for some drivers

--- shaders/skybox.fs
+++ shaders/skybox.fs
@@ -9,6 +9,13 @@ uniform sampler2D textures[6];
 
 void main()
 {
-	vec3 color = vec3(texture(textures[int(texIndex)], texCoord));
+	vec3 color;
+	if (texIndex == 0.0) { color = vec3(texture(textures[0], texCoord));
+	} else if (texIndex == 1.0) { color = vec3(texture(textures[1], texCoord));
+	} else if (texIndex == 2.0) { color = vec3(texture(textures[2], texCoord));
+	} else if (texIndex == 3.0) { color = vec3(texture(textures[3], texCoord));
+	} else if (texIndex == 4.0) { color = vec3(texture(textures[4], texCoord));
+	} else if (texIndex == 5.0) { color = vec3(texture(textures[5], texCoord));
+	}
 	FragColor = vec4(color, 1.0);
 }
